2f9cf6b73cbe26c261a1b4647678dca52ab95475
garytyree-dc <garytyree.dc@gmail.com>
2017-11-10 16:35:59 -0500

    update output

--- server-init.js
+++ server-init.js
@@ -2,7 +2,7 @@ const port = 3000
 require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
-    res.end('hello world')
+    res.end('Hello, World. This is GT')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
